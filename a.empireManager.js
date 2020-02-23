a817b75710833e72b54b8219391340a18cf70bb4
Juston Hart <justonhart@yahoo.com>
2020-02-23 12:59:19 -0600

    Commented out spawning code that was room specific; there needs to be smart determination for combat creeps

--- a.empireManager.js
+++ a.empireManager.js
@@ -2,58 +2,58 @@
  * Manages empire; potential features include central distribution of energy using terminals?
  */
 var popControl = require('s.population');
- 
+
 var empireManager = {
-    
+
     run: function(){
-        
+
         manageCreepMemoryClearing();
-        
+
         //creep spawning mechanism
         for(let spawnName in Game.spawns){
             popControl.run(Game.spawns[spawnName]);
         }
-        
+
         //expansion code
         let spawn = Game.spawns.Spawn3;
-        
+
         if(Game.flags.claimFlag && !Memory.empire.colonyLaunchRoom)
             chooseRoomForColonyLaunch();
-        
+
         if(Game.flags.demoFlag1 && !Memory.empire.demoSpawnRoom)
             chooseDemoSpawn();
-        
-        if(Game.flags.attackFlag && (_.filter(Game.creeps, (creep) => (creep.memory.role == 'invader'))).length < 1)
-            Game.spawns.Spawn4.spawnCreep([TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE],"invader"+Game.time, {memory: {role: 'invader', home: Game.spawns.Spawn.room.name}});
-        if(Game.flags.attackFlag && (_.filter(Game.creeps, (creep) => (creep.memory.role == 'invasionHealer'))).length < 1)
-            Game.spawns.Spawn7.spawnCreep([HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE],"healer"+Game.time, {memory: {role: 'invasionHealer', home: Game.spawns.Spawn.room.name}});
-        
-        if(Game.flags.tankFlag && (_.filter(Game.creeps, (creep) => (creep.memory.role == 'invasionTank'))).length < 1)
-            Game.spawns.Spawn7.spawnCreep([ATTACK, ATTACK, ATTACK, ATTACK, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE],"lel @ "+Game.time, {memory: {role: 'invasionTank', home: Game.spawns.Spawn.room.name}});
-        
-        if(Game.flags.harassFlag && (_.filter(Game.creeps, (creep) => (creep.memory.role == 'harasser'))).length < 1)
-            Game.spawns.Spawn9.spawnCreep([ATTACK,ATTACK,ATTACK,MOVE,MOVE,MOVE,RANGED_ATTACK,RANGED_ATTACK,RANGED_ATTACK,MOVE,MOVE,MOVE], "harasser"+Game.time, {memory: {role: 'harasser', home: Game.spawns.Spawn5.room.name}});
-        
-        if(Game.flags.mouseFlag && (_.filter(Game.creeps, (creep) => (creep.memory.role == 'mouse'))).length < 1)
-            Game.spawns.Spawn9.spawnCreep([MOVE], "mouse"+Game.time, {memory: {role: 'mouse', home: Game.spawns.Spawn5.room.name}});
+
+        // if(Game.flags.attackFlag && (_.filter(Game.creeps, (creep) => (creep.memory.role == 'invader'))).length < 1)
+        //     Game.spawns.Spawn4.spawnCreep([TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, ATTACK, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE],"invader"+Game.time, {memory: {role: 'invader', home: Game.spawns.Spawn.room.name}});
+        // if(Game.flags.attackFlag && (_.filter(Game.creeps, (creep) => (creep.memory.role == 'invasionHealer'))).length < 1)
+        //     Game.spawns.Spawn7.spawnCreep([HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE],"healer"+Game.time, {memory: {role: 'invasionHealer', home: Game.spawns.Spawn.room.name}});
+        //
+        // if(Game.flags.tankFlag && (_.filter(Game.creeps, (creep) => (creep.memory.role == 'invasionTank'))).length < 1)
+        //     Game.spawns.Spawn7.spawnCreep([ATTACK, ATTACK, ATTACK, ATTACK, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, HEAL, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE],"lel @ "+Game.time, {memory: {role: 'invasionTank', home: Game.spawns.Spawn.room.name}});
+        //
+        // if(Game.flags.harassFlag && (_.filter(Game.creeps, (creep) => (creep.memory.role == 'harasser'))).length < 1)
+        //     Game.spawns.Spawn9.spawnCreep([ATTACK,ATTACK,ATTACK,MOVE,MOVE,MOVE,RANGED_ATTACK,RANGED_ATTACK,RANGED_ATTACK,MOVE,MOVE,MOVE], "harasser"+Game.time, {memory: {role: 'harasser', home: Game.spawns.Spawn5.room.name}});
+        //
+        // if(Game.flags.mouseFlag && (_.filter(Game.creeps, (creep) => (creep.memory.role == 'mouse'))).length < 1)
+        //     Game.spawns.Spawn9.spawnCreep([MOVE], "mouse"+Game.time, {memory: {role: 'mouse', home: Game.spawns.Spawn5.room.name}});
     }
 }
 
 function manageCreepMemoryClearing(){
     //memory management
     for(var name in Memory.creeps) {
-        
+
         var deadCreepMemory = Memory.creeps[name];
-        
+
         if(!Game.creeps[name]) {
-            
+
             console.log('Clearing non-existing creep memory:', name);
             if(deadCreepMemory.role)
                 try{clearCreepAssignments(deadCreepMemory);}catch(error){console.log(name + ": error clearing assignment")}
-            
+
             if(deadCreepMemory.miningPos)
                 Game.rooms[deadCreepMemory.home].memory.sourceAccessPoints.push(deadCreepMemory.miningPos);
-            
+
             delete Memory.creeps[name];
         }
     }
@@ -83,12 +83,12 @@ function clearCreepAssignments(deadCreepMemory){
     }
 }
 
-//this function will spawn creeps at the correct room to 
+//this function will spawn creeps at the correct room to
 function chooseRoomForColonyLaunch(){
     let flag = Game.flags.claimFlag;
-    
+
     let possibleSpawnRooms = _.filter(Game.rooms, function(r){return r.controller && r.controller.my && r.economyStatus >= ECON_STABLE;});
-    
+
     let minDistance = 11;
     let spawnRoom;
     for(let i in possibleSpawnRooms){
@@ -98,21 +98,21 @@ function chooseRoomForColonyLaunch(){
             spawnRoom = possibleSpawnRooms[i].name;
         }
     }
-    
+
     if(spawnRoom){
         Memory.empire.colonyLaunchRoom = spawnRoom;
         Memory.empire.colonyRoomUnclaimed = true;
     }
     else
         console.log("No suitable spawn room for claimer");
-    
+
 }
 
 function chooseDemoSpawn(){
     let flag = Game.flags.demoFlag1;
-    
+
     let possibleSpawnRooms = _.filter(Game.rooms, function(r){return r.controller && r.controller.my && r.economyStatus >= ECON_STABLE;});
-    
+
     let minDistance = 11;
     let spawnRoom;
     for(let i in possibleSpawnRooms){
