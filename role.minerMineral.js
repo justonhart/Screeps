e2f5115cab775189ed4490f4f097442b0d044ed5
Juston Hart <justonhart@yahoo.com>
2020-03-05 20:32:20 -0600

    Slightly change mineral miner behavior to place construction site if one hasn't been placed

--- role.minerMineral.js
+++ role.minerMineral.js
@@ -7,6 +7,20 @@ var roleMineralMiner = {
         }
         
         var destination = Game.getObjectById(creep.memory.destination);
+        if(creep.pos.isNearTo(Game.getObjectById(creep.memory.assignment))){
+            if(Game.getObjectById(creep.memory.destination) instanceof Mineral){
+                creep.pos.createConstructionSite(STRUCTURE_CONTAINER);
+                delete creep.memory.destination;
+            }
+            else{
+                creep.harvest(Game.getObjectById(creep.memory.assignment));
+            }
+        }
+        else{
+            creep.moveTo(destination);
+        }
+        
+        /*
         if(destination.structureType == STRUCTURE_CONTAINER && _.sum(destination.store) < 2000){
             switch(creep.harvest(Game.getObjectById(creep.memory.assignment))){
                 case ERR_NOT_IN_RANGE:
@@ -14,13 +28,17 @@ var roleMineralMiner = {
                     break;
             }
         }
+        else{
+            creep.moveTo(destination);
+        }
+        */
     }
 };
 
 function calculateDestination(creep){
    
     creep.memory.assignment = creep.room.find(FIND_MINERALS)[0].id;
-    //look for container near source and move there; if none, move to source and drop container site
+    //look for container near source and move there; if none, move to source
     var source = Game.getObjectById(creep.memory.assignment);
     var container = _.find(creep.room.lookForAtArea(LOOK_STRUCTURES, source.pos.y - 1, source.pos.x - 1, source.pos.y + 1, source.pos.x + 1, true), s=> {return s.structure.structureType == STRUCTURE_CONTAINER});
     if(container)
