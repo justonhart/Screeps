ee162a785caece1d46cd22a226749028e931038c
Juston Hart <contact@justonh.art>
2022-03-14 08:21:41 -0500

    Attempt to fix issue in maintenance
    Maintenance didn't have any local code that set gathering to true,
    hopefully this will fix the issue causing them to get stuck w/o any
    energy

--- src/role.maintenance.js
+++ src/role.maintenance.js
@@ -5,15 +5,23 @@ var roleMaintenance = {
     run: function(creep) {
 
         if(creep.memory.gathering === false) {
-            var targets =  _.sortBy(creep.room.find(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType == STRUCTURE_RAMPART && structure.hits < 100)
-            || (structure.structureType !== STRUCTURE_RAMPART && structure.structureType !== STRUCTURE_WALL) && structure.hits < structure.hitsMax}}), s => s.hits);
-            
-            if(targets.length != 0) {
-                if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) 
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+
+            if(creep.store[RESOURCE_ENERGY] === 0){
+                creep.memory.gathering = true;
+                creep.gatherEnergy();
             }
-            else
-                roleUpgrader.run(creep);
+
+            else{
+                var targets =  _.sortBy(creep.room.find(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType == STRUCTURE_RAMPART && structure.hits < 100)
+                    || (structure.structureType !== STRUCTURE_RAMPART && structure.structureType !== STRUCTURE_WALL) && structure.hits < structure.hitsMax}}), s => s.hits);
+                    
+                if(targets.length != 0) {
+                    if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) 
+                        creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+                else
+                    roleUpgrader.run(creep);
+            }
         }
 	    else {
             creep.gatherEnergy();
