dd5aaf87cd9667a90ddf50a4baae86ddff5c286b
Juston Hart <contact@justonh.art>
2022-03-19 20:41:18 -0500

    Add responsibilities to harversters and builders

--- src/role.builder.js
+++ src/role.builder.js
@@ -6,13 +6,11 @@ var roleBuilder = {
     run: function(creep) {
         if(creep.memory.gathering === false) {
             if(!creep.memory.target){
-                let site = creep.room.find(FIND_CONSTRUCTION_SITES, {filter: s=>{return s.structureType !== STRUCTURE_WALL && s.structureType !== STRUCTURE_RAMPART;}})[0];
-                if(site)
-                    creep.memory.target = site.id;
+                creep.memory.target = findTarget(creep);
             }
             
             var target = Game.getObjectById(creep.memory.target);
-            if(target) {
+            if(target instanceof ConstructionSite) {
                 switch(creep.build(target)){
                     case ERR_NOT_IN_RANGE:
                         creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
@@ -25,9 +23,18 @@ var roleBuilder = {
                         break;
                 }
             }
+            else if(target instanceof Structure){
+                switch(creep.repair(target)){
+                    case ERR_NOT_IN_RANGE:
+                        creep.moveTo(target, {visualizePathStyle: {stroke: '#ff9900', opacity: .5}});
+                        break;
+                    case ERR_INVALID_TARGET:
+                        delete creep.memory.targetId;
+                        break;
+                }
+            }
             else{
                 creep.memory.target = null;
-                //roleMason.run(creep);
             }
         }
         
@@ -36,5 +43,34 @@ var roleBuilder = {
         }
     }
 };
+
+function findTarget(creep){
+    let sites = creep.room.find(FIND_CONSTRUCTION_SITES, {filter: s=>{return s.structureType !== STRUCTURE_WALL && s.structureType !== STRUCTURE_RAMPART;}});
+    if(sites.length){
+        sites.sort((a,b) => b.progress - a.progress);
+        return sites[0].id;
+    }
+
+    let myStructures = creep.room.find(FIND_STRUCTURES, {filter: s => s.hits < s.hitsMax && !(s.structureType === STRUCTURE_WALL || s.structureType === STRUCTURE_RAMPART)});
+    
+    if(myStructures.length){
+        //sort ascending by health ratio
+        myStructures.sort((a,b) => (a.hits/a.hitsMax) - (b.hits/b.hitsMax));
+
+        //take only the lowest ratio and find the closest target among them
+        let slice = myStructures.filter(s => (s.hits / s.hitsMax) === (myStructures[0].hits / myStructures[0].hitsMax) )
+        return creep.pos.findClosestByPath(slice).id;
+    }
+
+    let walls = creep.room.find(FIND_STRUCTURES).filter(s => (s.structureType === STRUCTURE_WALL || s.structureType === STRUCTURE_RAMPART) && s.hits < creep.room.getDefHP())
+    if(walls.length){
+        //sort by lowest hits
+        walls.sort((a,b) => a.hits - b.hits);
+
+        //take only the lowest ratio and find the closest target among them
+        let slice = walls.filter(s => s.hits === walls[0].hits);
+        return creep.pos.findClosestByPath(slice).id;
+    }
+}
 
 module.exports = roleBuilder;
--- src/role.harvester.js
+++ src/role.harvester.js
@@ -22,28 +22,52 @@ var roleHarvester = {
                 }
             }
 
+            else if(target instanceof ConstructionSite){
+                switch(creep.build(target)){
+                    case ERR_NOT_IN_RANGE:
+                        creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+                        break;
+                    case ERR_INVALID_TARGET:
+                        creep.memory.target = null;
+                        break;
+                    case ERR_NOT_ENOUGH_RESOURCES:
+                        creep.memory.gathering = true;
+                        break;
+                }
+            }
+
             else if(target instanceof StructureController){
                 if(creep.pos.getRangeTo(creep.room.controller) > 3)
                     creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
                 else
                     creep.upgradeController(creep.room.controller);
             }
-      }
-      else{
-          creep.memory.target = null;
-          creep.gatherEnergy();
-      }
+        }
+        else{
+            creep.memory.target = null;
+            creep.gatherEnergy();
+        }
 	}
 };
 
 function findTarget(creep){
     var targets = creep.room.find(FIND_MY_STRUCTURES, { filter: structure => { return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) && structure.energy < structure.energyCapacity
         || (structure.structureType == STRUCTURE_TOWER && structure.energy < 700)}});
-    var target = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {filter: structure => {return _.find(targets, structure)}});
+    
 
-    if(target)
+    if(targets.length){
+        let target = creep.pos.findClosestByPath(targets);
         creep.memory.target = target.id;
+        return;
+    }
 
+    let sites = creep.room.find(FIND_CONSTRUCTION_SITES, {filter: s=>{return s.structureType !== STRUCTURE_WALL && s.structureType !== STRUCTURE_RAMPART;}});
+    if(sites.length){
+        let site = creep.pos.findClosestByPath(sites);
+        creep.memory.target = site.id;
+        return;
+    }
+    
     else if(creep.room.operatingMode != ECO_MODE && creep.room.storage && creep.room.storage.my)
         creep.memory.target = creep.room.storage.id;
 
